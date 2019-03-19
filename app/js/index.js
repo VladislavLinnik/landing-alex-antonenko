1bc9b047a72dc249adc9683fe9298bcec92c68a1
Vladislav Linnik <vladik.linnik.1999@mail.ru>
2019-03-20 00:45:57 +0200

    some shanges

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,27 @@
-"use strict"
+$(document).ready(function(){
+	// ПЛАВНЫЙ ПЕРЕХОД ПО ЯКОРЯМ
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
 
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+	
+	// $( '.nav-toggle' ).click( function() {
+    //     if ( $( '#menu' ).is( ':hidden' ) ) {
+    //         $( '#menu' ).show('slideToggle');
+    //     }
+    //     else {
+	//         $( '#menu' ).hide('slideToggle');
+    //     }
+    // });
+
+});
